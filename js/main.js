691d62ebcb7b50fff8d3bbfac0acea250efb570b
feffyx <ziacofederica@gmail.com>
2023-11-27 11:26:01 +0100

    added discount codes + bonus, edited HTML, edited Language

--- js/main.js
+++ js/main.js
@@ -16,12 +16,32 @@ document.addEventListener('DOMContentLoaded', function () {
         };
 
         if (!(workType in hourlyPrices)) {
-            alert('Tipo di lavoro non valido');
+            alert('Working type not valid');
             return;
         }
 
         let hourlyPrice = hourlyPrices[workType];
         let total = hours * hourlyPrice;
+
+        // CODICI SCONTO
+        let validCodes = ['YHDNU32', 'JANJC63', 'PWKCN25', 'SJDPO96', 'POCIE24'];
+
+        // SE IL CODICE È VALIDO, SI APPLICA UNO SCONTO DEL 25%
+        if (validCodes.includes(discountCode)) {
+            let discount = total * 0.25;
+            total -= discount;
+  
+        
+        } else if (discountCode !== '') {
+            alert('Discount code invalid');
+
+            discountCodeInput.style.color = 'red'; // da il colore rosso al codice non valido
+        }
+
+        // MOSTRA PREZZO SOTTO AL FORM
+        let resultElement = document.createElement('p');
+        resultElement.innerHTML = 'Final price: ' + total.toFixed(2) + '€';
+        document.querySelector('form').appendChild(resultElement);
 
 
     });
